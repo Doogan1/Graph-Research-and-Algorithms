1a4cfb93c108a3e9d5afb0d99add1dc0988e692d
Drake Olejniczak <48662393+Doogan1@users.noreply.github.com>
2024-03-22 22:12:39 -0400

    Update script.js
    fixed fetch path to graphs

--- Down Arrow Ramsey Set of a Graph/script.js
+++ Down Arrow Ramsey Set of a Graph/script.js
@@ -29,7 +29,7 @@ const updateCurrentGraph = async () => {
             [...parameterInputs][0].value = "";
             return;
         } 
-        const res = await fetch(`Down Arrow Ramsey Set of a Graph/graphs/${selectedGraph}_${parameterArr[0]}_data.json`);
+        const res = await fetch(`https://doogan1.github.io/Graph-Research-and-Algorithms/Down%20Arrow%20Ramsey%20Set%20of%20a%20Graph/graphs/${selectedGraph}_${parameterArr[0]}_data.json`);
         const graphData = await res.json();
         currentGraph[`graphData`] = graphData;
             //reset();
@@ -61,7 +61,7 @@ const updateCurrentGraph = async () => {
                 return;
             }
         }
-        const res = await fetch(`Down Arrow Ramsey Set of a Graph/graphs/complete_bipartite_graph_${parameterArr[0]}_${parameterArr[1]}_data.json`);
+        const res = await fetch(`https://doogan1.github.io/Graph-Research-and-Algorithms/Down%20Arrow%20Ramsey%20Set%20of%20a%20Graph/graphs/complete_bipartite_graph_${parameterArr[0]}_${parameterArr[1]}_data.json`);
         const graphData = await res.json();
         currentGraph['graphData'] = graphData;
             //reset();
@@ -72,7 +72,7 @@ const updateCurrentGraph = async () => {
         //setupZoomAndPan('graph-container');
     } else {
         showRamseyBtn.disabled = true;
-        const res = await fetch(`Down Arrow Ramsey Set of a Graph/graphs/${selectedGraph}_data.json`);
+        const res = await fetch(`https://doogan1.github.io/Graph-Research-and-Algorithms/Down%20Arrow%20Ramsey%20Set%20of%20a%20Graph/graphs/${selectedGraph}_data.json`);
         const graphData = await res.json();
         currentGraph['graphData'] = graphData;
             //reset();
